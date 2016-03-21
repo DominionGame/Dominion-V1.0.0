078a14ce744edcd4e52e264736c996bc56c2fbec
thomasdenil <thomasdenil148@gmail.com>
2016-03-21 13:12:42 +0100

    Added files via upload

---
+++ DominionGame/src/DominionServlet.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import javax.servlet.*;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DominionServlet
+ */
+public class DominionServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	protected void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+	 response.setContentType("text/html");
+	 response.setCharacterEncoding("utf-8");
+	 PrintWriter out = response.getWriter();
+	 out.println("<html><body>Dominion Test</body></html>");
+	 
+	 //request Dispatcher om html files mee te geven 
+	}
+	
+
+}
